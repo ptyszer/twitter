47cfca3b645e562518e98545d54a6ee3ed01bc6a
ptyszer <p.tyszer@hotmail.com>
2017-12-17 15:02:39 +0100

    fix

--- users.sql
+++ users.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `users` (
+CREATE TABLE `Users` (
     `id` INT AUTO_INCREMENT,
     `email` VARCHAR(255) UNIQUE NOT NULL,
     `username` VARCHAR(255) NOT NULL,
-    `$hash_pass` VARCHAR(255) NOT NULL,
+    `hash_pass` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`)
 );
