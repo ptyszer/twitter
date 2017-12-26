41b657db0cb6be1cba7d033195f8615bb512cb03
ptyszer <p.tyszer@hotmail.com>
2017-12-26 20:54:16 +0100

    added all tweets display

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 require_once 'src/connection.php';
+require_once 'src/User.php';
 require_once 'src/Tweet.php';
 session_start();
 
-if (!isset($_SESSION['user'])){
+if (!isset($_SESSION['user'])) {
     header("Location: web/login.php");
 }
 
@@ -21,26 +22,46 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $tweet->saveToDB($conn);
 
 }
+
+$allTweets = Tweet::loadAllTweets($conn);
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" type="text/css" href="src/style.css">
     <meta charset="UTF-8">
     <title>Strona główna</title>
 </head>
 <body>
-<h>Strona główna</h>
-<br>
-<br><a href='web/login.php?action=logout'>Wyloguj się</a><br>
-<br>
-<form action="" method="post" role="form">
-
-    <label for="message">Your tweet:<br></label>
-    <textarea cols="40" rows="6" id="text" name="text" placeholder="tweet.."></textarea>
+<div class="container">
+    <h><a href="index.php">Strona główna</a></h>
+    <br>
+    <br><a href='web/login.php?action=logout'>Wyloguj się</a><br>
     <br>
 
-    <input type="submit" value="Send">
-</form>
+    <div class="content">
+        <div class="tweet-form">
+            <form action="" method="post" role="form">
+
+                <textarea cols="40" rows="4" id="text" name="text" placeholder="What's happening?"></textarea>
+                <br>
+
+                <input type="submit" value="Tweet">
+            </form>
+        </div>
+        <?php
+        foreach ($allTweets as $t) {
+            $userId = $t->getUserId();
+            $user = User::loadUserById($conn, $userId);
+            echo "<div class='tweet'>";
+            echo $user->getUsername() . " - " . $t->getCreationDate() . "<br>";
+            echo $t->getText() . "<br>";
+            echo "</div>";
+        }
+        ?>
+    </div>
+</div>
 </body>
 </html>
